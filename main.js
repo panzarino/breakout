5e64e36524005d9a35d7e2f988cdcd14d60b4205
Zach Panzarino <zachary@panzarino.com>
2016-01-12 18:25:36 +0000

    Bouncing ball

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 var canvas = document.getElementById("game");
 var ctx = canvas.getContext("2d");
 
-// get starting position
+// get starting position for ball
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 
+// ball variables
+var ballRadius = 10;
+
 // draw the ball
 function drawBall(){
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -22,6 +25,14 @@ function draw(){
     drawBall();
     x += dx;
     y += dy;
+    
+    // bounce off the walls
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
 }
 
 // redraw every 10 milliseconds
